645a354c37f7fb2c417e94fba591fa098b664031
Nina-Jasmine <liunina98@hotmail.com>
2022-04-18 08:59:07 -0400

    Question 2 complete

--- src/main/java/com/example/w22comp1011gctest2student/Product.java
+++ src/main/java/com/example/w22comp1011gctest2student/Product.java
@@ -1,9 +1,48 @@
 package com.example.w22comp1011gctest2student;
 
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 public class Product {
+  @SerializedName("id")
+   private int productId;
 
+  private String name;
 
+   private String sku;
+   private double salePrice;
+   private double regularPrice;
+  @SerializedName("image")
+   private String imageUrl;
+
+  public int getProductId() {
+    return productId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSku() {
+    return sku;
+  }
+
+  public double getSalePrice() {
+    return salePrice;
+  }
+
+  public double getRegularPrice() {
+    return regularPrice;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s - $%.2f", name, salePrice);
 
   }
+}
